f67127ce66bea8dc6c50d3936f6b943720b59dc7
Lahiru Dilshan <lahirud608@gmail.com>
2024-11-30 10:30:20 +0530

    maintained

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
