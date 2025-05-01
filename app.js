19d4fdc6b9a5ce5b994604484024aadd7998df71
Lahiru Dilshan <lahirud608@gmail.com>
2025-05-02 04:48:48 +0530

    update files

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
-const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 const sass = require("sass");
 
-function installUIFramework() {
-  try {
-    console.log("Running UI-Framework Installation");
-    console.log("Converting scss to css");
-    const scssFilePath = sass.compile("packages/scss/core.scss");
-    fs.mkdirSync("C:/Users/lahir/Desktop/UI-Framework-/dist/packages", {
-      recursive: true,
-    });
-    fs.writeFileSync(
-      "C:/Users/lahir/Desktop/UI-Framework-/dist/packages/core.css",
-      scssFilePath.css
-    );
-    const sourceTailwindConfig = path.join(__dirname, "uif.config.js");
-    const destTailwindConfig = path.join(__dirname, "dist", "uif.config.js");
-    fs.copyFileSync(sourceTailwindConfig, destTailwindConfig);
-    console.log("UI-Framework Installation Completed");
-  } catch (error) {
-    console.error(error.message);
+const DIST_DIR = path.join(__dirname, "dist");
+const SCSS_ENTRY_FILE = path.join(__dirname, "packages", "scss");
+const SOURCE_CONFIG_FILE = path.join(__dirname, "zyntra.config.js");
+const DEST_CONFIG_FILE = path.join(DIST_DIR, "zyntra.config.js");
+
+function Make_Dist_Directory() {
+  if (!fs.existsSync(DIST_DIR)) {
+    fs.mkdirSync(DIST_DIR, { recursive: true });
+    console.log("+ ", DIST_DIR);
+  } else {
+    console.log("! ", DIST_DIR);
   }
 }
 
-installUIFramework();
+function Compile_SCSS_To_CSS() {
+  const SCSS_FILES = fs
+    .readdirSync(SCSS_ENTRY_FILE)
+    .filter((file) => file.endsWith(".scss"));
+  for (const file of SCSS_FILES) {
+    const INPUT_PATH = path.join(SCSS_ENTRY_FILE, file);
+    const OUTPUT_FILE_NAME = path.join(DIST_DIR, file.replace(".scss", ".css"));
+    const OUTPUT_PATH = path.join(OUTPUT_FILE_NAME);
+
+    try {
+      const COMPILER = sass.compile(INPUT_PATH);
+      console.log("+ ", INPUT_PATH, "->", OUTPUT_PATH);
+      fs.writeFileSync(OUTPUT_PATH, COMPILER.css);
+    } catch (error) {
+      console.error("! ", error.message);
+    }
+  }
+}
+
+function Copy_Config_File() {
+  try {
+    if (!fs.existsSync(SOURCE_CONFIG_FILE)) {
+      console.error("! ", SOURCE_CONFIG_FILE);
+      return;
+    }
+
+    if (!fs.existsSync(DEST_CONFIG_FILE)) {
+      fs.copyFileSync(SOURCE_CONFIG_FILE, DEST_CONFIG_FILE);
+      console.log("+ ", DEST_CONFIG_FILE);
+    } else {
+      console.log("! ", DEST_CONFIG_FILE);
+    }
+  } catch (error) {
+    console.error("! ", error.message);
+  }
+}
+
+function main() {
+  Make_Dist_Directory();
+  Compile_SCSS_To_CSS();
+  // Copy_Config_File();
+}
+
+main();
